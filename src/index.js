79074ba4239f7e3ea3baa0243b33a30337893c1f
spelaskof <spela.skof98@gmail.com>
2022-08-05 10:21:10 +0200

    Correct the missed word and # in index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
-          }d@2x.png"
+          }@2x.png"
           alt=""
           width="42"
         />
@@ -72,7 +72,7 @@ function getForecast(coordinates) {
   let apiKey = "94282537c996f720e1352f577f55f09f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(dispayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function dispayCondition(response) {
@@ -81,7 +81,7 @@ function dispayCondition(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("date");
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
